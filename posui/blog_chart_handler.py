233c4599d5635c5132da30ce597f909c5cdde795
Michael Kang <michael3dk@gmail.com>
2023-03-19 12:57:15 -0700

    changed the URL to parse BlogChart themes

--- posui/blog_chart_handler.py
+++ posui/blog_chart_handler.py
@@ -1,4 +1,4 @@
-from enum import Enum
+import re
 from urllib.parse import urlparse
 
 import feedparser
@@ -8,6 +8,7 @@ from posui.platforms.common import fetch_soup
 
 class BlogChartHandler:
     def __init__(self, themes):
+        self._all_themes = self.__generate_themes_list()
         self._themes = themes
         self._ranks = self.__find_ranks(self._themes)
 
@@ -15,22 +16,44 @@ class BlogChartHandler:
     def ranks(self):
         return self._ranks
 
+    @property
+    def all_theme_names(self):
+        return self._all_themes.keys()
+
+    def __generate_themes_list(self):
+        all_themes = {}
+
+        # URL = "https://www.blogchart.co.kr/chart/theme"
+        # soup = fetch_soup(URL)
+        # found = soup.select("ul.theme_list li a")
+        # for row in found:
+        #     theme = row.select_one("p").text
+        #     url = row.get("href")
+        #     all_themes[theme] = url
+
+        URL2 = "https://www.blogchart.co.kr/chart/theme_list"
+
+        soup = fetch_soup(URL2)
+        area = soup.select_one("table.Category_list_table")
+        found = area.select("a")
+        for row in found:
+            theme = row.text
+            url = self.__convert_link(row)
+            if theme in all_themes and all_themes[theme] != url:
+                breakpoint()
+
+            all_themes[theme] = url
+
+        return all_themes
+
     def __find_ranks(self, themes):
-        URL = "https://www.blogchart.co.kr/chart/theme"
-
-        soup = fetch_soup(URL)
-        found = soup.select("ul.theme_list li a")
-        all_themes = [
-            {"theme": row.select_one("p").text, "url": row.get("href")} for row in found
-        ]
-
         ranks = {}
-        for row in all_themes:
-            if row["theme"] not in themes:
+        for theme in themes:
+            if theme not in self._all_themes.keys():
                 continue
 
-            url = row["url"]
-            ranks[row["theme"]] = self.__ranks(url)
+            url = self._all_themes.get(theme)
+            ranks[theme] = self.__ranks(url)
 
         return ranks
 
@@ -45,3 +68,14 @@ class BlogChartHandler:
         )
 
         return ranks
+
+    def __convert_link(self, tag):
+        # text = "goCate("ZGxxdWMkMkFwbl40IS4+Ympna2tjLHFvXjcgMysgMi8tPyAxQQ=="); return false;"
+        # return "https://www.blogchart.co.kr/chart/theme_list?theme=ZGxxdWMkMkFwbl40IS4+Ympna2tjLHFvXjcgMysgMi8tPyAxQQ=="
+        text = tag.get("onclick")
+        mo = re.search(r"\"(.*)\"", text)
+        if mo:
+            theme_name = mo[1]
+            return f"https://www.blogchart.co.kr/chart/theme_list?theme={theme_name}"
+
+        return None
--- scripts/test/run_blogchart.py
+++ scripts/test/run_blogchart.py
@@ -7,14 +7,18 @@ def main():
     with open("scripts/test/expected_blog_chart.yaml") as file:
         expected_list = yaml.safe_load(file)
 
-    themes = ["국내여행", "해외여행"]
+    themes = ["국내여행", "해외여행", "방송/연애"]
     bch = BlogChartHandler(themes)
 
     for idx, expected in enumerate(expected_list):
         try:
             theme = expected["theme"]
             print(f">>>{idx} {theme}")
-            actual = bch.ranks[theme]
+            actual = bch.ranks.get(theme)
+
+            if not actual:
+                continue
+
             for row in actual:
                 print(row)
 
