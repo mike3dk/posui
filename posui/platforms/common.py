b5fe6986a1ebac3558da56a5745d4977ca78fd37
Michael Kang <michael3dk@gmail.com>
2023-10-03 22:36:40 -0700

    removed SECLEVEL=1 from DEFAULT_CIPHERS
    ```
    File "posui/platforms/common.py", line 6, in fetch_soup
        requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS += "@SECLEVEL=1"
    AttributeError: module 'urllib3.util.ssl_' has no attribute 'DEFAULT_CIPHERS'
    ```

--- posui/platforms/common.py
+++ posui/platforms/common.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 
 def fetch_soup(url):
-    requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS += "@SECLEVEL=1"
+    # requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS += "@SECLEVEL=1"
     page = requests.get(url, timeout=300)  # 300 sec timeout
     soup = BeautifulSoup(page.content, "html.parser")
     return soup
